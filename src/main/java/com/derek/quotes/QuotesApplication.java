be975625a61fb6341b2d2cb030b8d19a4bacd04f
dms91 <drk7891@gmail.com>
2019-08-14 12:04:55 -0400

    removed scheduling

--- src/main/java/com/derek/quotes/QuotesApplication.java
+++ src/main/java/com/derek/quotes/QuotesApplication.java
@@ -2,10 +2,8 @@ package com.derek.quotes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling
 public class QuotesApplication {
 
 	public static void main(String[] args) {
